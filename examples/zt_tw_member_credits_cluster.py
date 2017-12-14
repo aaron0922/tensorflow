c0a8a519dc18dacf1aa5b0b6214e46fabd90ccea
aaron0922 <aaron0922@gmail.com>
2017-12-14 18:33:09 +0800

    test member credits&posts

--- examples/zt_tw_member_credits_cluster.py
+++ examples/zt_tw_member_credits_cluster.py
@@ -39,30 +39,24 @@ hw_frame = pd.read_csv(
 #hw_frame.drop('Index', 1, inplace=True)
 hw_frame.drop('uid', 1, inplace=True)
 print(hw_frame.head(5))
-'''
-print(hw_frame.views.count())
-print(hw_frame.views.mean())
-print(hw_frame.views.std())
-print(hw_frame.replies.count())
-print(hw_frame.replies.mean())
-print(hw_frame.replies.std())
-'''
-ScatterPlot(hw_frame.extcredits2, hw_frame.posts)
-#print(hw_frame[hw_frame.views > abs(-176)].head(5))
-#print(hw_frame[(hw_frame.views >= abs(-176)) & (hw_frame.replies == 2)].head(5))
-'''
-hw_frame = hw_frame[(abs(hw_frame.views - 310) < 5344) & (abs(hw_frame.replies - 8) < 132)]
-print(hw_frame.head(5))
-print(hw_frame.views.count())
-print(hw_frame.replies.count())
-#ScatterPlot(df.views, df.replies)
-'''
-'''
+
+count = hw_frame.extcredits2.count()
+print(count)
+mean1 = hw_frame.extcredits2.mean()
+std1 = hw_frame.extcredits2.std()
+mean2 = hw_frame.posts.mean()
+std2 = hw_frame.posts.std()
+hw_frame = hw_frame[(abs(hw_frame.extcredits2 - mean1) < std1*3) & (abs(hw_frame.posts - mean2) < std2*3) & (hw_frame.extcredits2 >=0)]
+print(hw_frame.extcredits2.count())
+#ScatterPlot(hw_frame.extcredits2, hw_frame.posts)
+
+
+
 b = time.time()
 print('b-a:'+str(b-a))
 tf.logging.set_verbosity(tf.logging.ERROR)
 
-kmeans = tf.contrib.learn.KMeansClustering(num_clusters=3, relative_tolerance=0.0001)
+kmeans = tf.contrib.learn.KMeansClustering(num_clusters=4, relative_tolerance=0.0001)
 c = time.time()
 print('c-b:'+str(c-b))
 _ = kmeans.fit(input_fn=input_fn)
@@ -77,8 +71,9 @@ assignments = list(kmeans.predict_cluster_idx(input_fn=input_fn))
 
 e = time.time()
 print('e-d:'+str(e-d))
-ScatterPlot(hw_frame.views, hw_frame.replies, assignments, clusters)
+#ScatterPlot(hw_frame.views, hw_frame.replies, assignments, clusters)
+ScatterPlot(hw_frame.extcredits2, hw_frame.posts, assignments, clusters)
 f = time.time()
 print('f-e:'+str(f-e))
-'''
+
 print('END')
